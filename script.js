8891f08b588684b3264680f660db71afe0f5c4a6
Christy Crook <ccrook3@gmail.com>
2021-12-07 17:18:43 -0600

    got start button to work

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let countdown = 0;
+let seconds = 0;
+let remaining =0;
+let isBreak = true;
+let isPaused = true;
 // Elements
 const pomodoroAppElement = document.getElementById("pomodoro-app");
 const pomodoroCounterDisplayElement = document.getElementById("pomodoroCounterDisplay");
@@ -9,3 +14,37 @@ const pomodoroListOfErrorsElement =document.getElementById("pomodoroErrorList");
 const addPomodoroToListButton = document.getElementById("pomorodoForm-addBtn");
 const pomodoroListElement = document.getElementById("PomodoroList");
 const startOrPauseAPomodoroFromListButton = document.getElementById("pomorodoForm-StartPauseBtn");
+
+// Start
+startOrPauseAPomodoroFromListButton.addEventListener('click', () => {
+    clearInterval(countdown);
+    if (isPaused) {
+        startCountdown();
+        startOrPauseAPomodoroFromListButton.textContent = "Pause";
+        isPaused = false;
+    } else {
+        remaining = seconds;
+        startOrPauseAPomodoroFromListButton.textContent = "Continue";
+        isPaused = true;
+    }
+})
+
+function timer() {  
+    seconds --;  if (seconds < 0) {    
+        clearInterval(countdown);    
+        alarm.currentTime = 0;    
+        alarm.play();    
+        seconds = (isBreak ? breakTime : workTime) * 60;    
+        isBreak = !isBreak;  
+    }
+}
+
+function startCountdown() {
+    if (remaining != 0) {
+      seconds = remaining;
+    } else {
+      seconds = minuteInputElement.textContent * 60;
+      pomodoroCounterDisplayElement.textContent = "Keep Working";
+    }
+    countdown = setInterval(timer, 1000);
+  }
