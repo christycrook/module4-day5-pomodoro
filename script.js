d6fea7fd3f2eafe0ed60e6b5a1ab570fda6c0528
Christy Crook <ccrook3@gmail.com>
2021-12-09 10:50:43 -0600

    updated js file

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
 let countdown = 0;
-let seconds = 0;
-let remaining =0;
+let seconds = 15000;
+let workTime
 let isBreak = true;
 let isPaused = true;
 // Elements
 const pomodoroAppElement = document.getElementById("pomodoro-app");
-const pomodoroCounterDisplayElement = document.getElementById("pomodoroCounterDisplay");
-const pomodoroFormElement = document.getElementsByClassName("pomorodoForm")[0];
-const minuteInputElement = document.getElementById("minutesInputText");
-const secondInputElement = document.getElementById("secondsInputText");
+const timerDisplay = document.getElementById("pomodoroCounterDisplay");
+const message = document.getElementById("pomodoroDescription");
+const workMin = document.getElementById("minutesInputText");
+const workSec = document.getElementById("secondsInputText");
 //pomodoroErrorList 
 const pomodoroListOfErrorsElement =document.getElementById("pomodoroErrorList");
-const addPomodoroToListButton = document.getElementById("pomorodoForm-addBtn");
+const addBtn = document.getElementById("pomorodoForm-addBtn");
 const pomodoroListElement = document.getElementById("PomodoroList");
-const startOrPauseAPomodoroFromListButton = document.getElementById("pomorodoForm-StartPauseBtn");
+const startBtn = document.getElementById("pomorodoForm-StartPauseBtn");
+const alarm = document.createElement('audio'); // A bell sound will play when the timer reaches 0
+alarm.setAttribute("src", "https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3");
 
 // Start
-startOrPauseAPomodoroFromListButton.addEventListener('click', () => {
+startBtn.addEventListener('click', () => {
     clearInterval(countdown);
-    if (isPaused) {
-        startCountdown();
-        startOrPauseAPomodoroFromListButton.textContent = "Pause";
-        isPaused = false;
-    } else {
-        remaining = seconds;
-        startOrPauseAPomodoroFromListButton.textContent = "Continue";
-        isPaused = true;
+    isPaused = !isPaused;
+    if (!isPaused) {
+      countdown = setInterval(timer, 1000);
     }
-})
+  })
 
-function timer() {  
-    seconds --;  if (seconds < 0) {    
-        clearInterval(countdown);    
-        alarm.currentTime = 0;    
-        alarm.play();    
-        seconds = (isBreak ? breakTime : workTime) * 60;    
-        isBreak = !isBreak;  
+  function timer() {
+    seconds --;
+    if (seconds < 0) {
+      clearInterval(countdown);
+      alarm.currentTime = 0;
+      alarm.play();
+      seconds = (isBreak ? breakTime : workTime) * 60;
+      isBreak = !isBreak;
+      countdown = setInterval(timer, 1000);
     }
-}
-
-function startCountdown() {
-    if (remaining != 0) {
-      seconds = remaining;
-    } else {
-      seconds = minuteInputElement.textContent * 60;
-      pomodoroCounterDisplayElement.textContent = "Keep Working";
-    }
-    countdown = setInterval(timer, 1000);
   }
+
+  function countdownDisplay() {
+    let minutes = Math.floor(seconds / 60);
+    let remainderSeconds = seconds % 60;
+    timerDisplay.textContent = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  }
+
+  function buttonDisplay() {
+    if (isPaused && countdown === 0) {
+      startBtn.textContent = "START";
+    } else if (isPaused && countdown !== 0) {
+      startBtn.textContent = "Continue"; 
+    } else {
+      startBtn.textContent = "Pause";
+    }
+  }
+
+  
