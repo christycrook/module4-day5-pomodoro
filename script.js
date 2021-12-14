7b2b6a3018baae2846283ca2e16a193db1ea3c7d
Christy Crook <ccrook3@gmail.com>
2021-12-14 14:51:38 -0600

    got the add and start button to work

--- script.js
+++ script.js
@@ -1,57 +1,110 @@
-let countdown = 0;
-let seconds = 15000;
-let workTime
-let isBreak = true;
-let isPaused = true;
 // Elements
-const pomodoroAppElement = document.getElementById("pomodoro-app");
-const timerDisplay = document.getElementById("pomodoroCounterDisplay");
-const message = document.getElementById("pomodoroDescription");
-const workMin = document.getElementById("minutesInputText");
-const workSec = document.getElementById("secondsInputText");
-//pomodoroErrorList 
-const pomodoroListOfErrorsElement =document.getElementById("pomodoroErrorList");
-const addBtn = document.getElementById("pomorodoForm-addBtn");
-const pomodoroListElement = document.getElementById("PomodoroList");
-const startBtn = document.getElementById("pomorodoForm-StartPauseBtn");
-const alarm = document.createElement('audio'); // A bell sound will play when the timer reaches 0
-alarm.setAttribute("src", "https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3");
+const pomodoroCounterDisplayElement = document.getElementById("pomodoroCounterDisplay");
+const minuteInputElement = document.getElementById("minutesInputText");
+const secondInputElement = document.getElementById("secondsInputText");
+const addPomodoroToListButton = document.getElementById("pomorodoForm-addBtn");
+const startOrPauseAPomodoroFromListButton = document.getElementById("pomorodoForm-StartPauseBtn");
+let timer;
+let totalMilliseconds;
 
-// Start
-startBtn.addEventListener('click', () => {
-    clearInterval(countdown);
-    isPaused = !isPaused;
-    if (!isPaused) {
-      countdown = setInterval(timer, 1000);
-    }
-  })
+minuteInputElement.addEventListener("keyup", inputFields);
+secondInputElement.addEventListener("keyup", inputFields);
+addPomodoroToListButton.addEventListener("click", addClick);
+startOrPauseAPomodoroFromListButton.addEventListener("click", addClick);
 
-  function timer() {
-    seconds --;
-    if (seconds < 0) {
-      clearInterval(countdown);
-      alarm.currentTime = 0;
-      alarm.play();
-      seconds = (isBreak ? breakTime : workTime) * 60;
-      isBreak = !isBreak;
-      countdown = setInterval(timer, 1000);
-    }
+
+function inputFields() {
+  let minute = Number(minuteInputElement.value)
+  let second = Number(secondInputElement.value)
+
+  if (minute < 60 && minute >= 0 && second < 60 && second >= 0) {
+    addPomodoroToListButton.disabled = false
+  } else {
+    addPomodoroToListButton.disabled = true
+  }
+};
+
+function addClick() {
+  let minute = Number(minuteInputElement.value)
+  let second = Number(secondInputElement.value)
+
+  const formatInput = populateFormatPomodoro(minute, second)
+  minuteInputElement.value = formatInput[0]
+  secondInputElement.value = formatInput[1]
+
+  startOrPauseAPomodoroFromListButton.disabled = false
+};
+
+function startOrPauseClick() {
+  const btnState = startOrPauseAPomodoroFromListButton.getAttribute("start-pause-state")
+
+  if (btnState == "start") startPomodoro()
+  if (btnState == "pause") pausePomodoro()
+};
+
+function startPomodoro() {
+  addPomodoroToListButton.disabled = true;
+  startOrPauseAPomodoroFromListButton.setAttribute("start-pause-state", "pause");
+  startOrPauseAPomodoroFromListButton.innerText = "pause";
+
+  let timeArray = pomodoroCounterDisplayElement.innerText.split(":");
+  let minute = Number(timeArray[0]);
+  let second = Number(timeArray[1]);
+
+  totalMilliseconds = minute * 60000 + second * 1000;
+
+  timer = setInterval(pomodoroCountdown, 1000);
+};
+
+function pausePomodoro() {
+  addPomodoroToListButton.disabled = false
+  startOrPauseAPomodoroFromListButton.setAttribute("start-pause-state", "start")
+  startOrPauseAPomodoroFromListButton.innerText = "start"
+  clearInterval(timer)
+};
+
+function pomodoroCountdown() {
+  totalMilliseconds -= 1000
+
+  let minute = 0
+  let second = 0
+
+  if (totalMilliseconds >= 60000) {
+    minute = parseInt(totalMilliseconds / 60000)
   }
 
-  function countdownDisplay() {
-    let minutes = Math.floor(seconds / 60);
-    let remainderSeconds = seconds % 60;
-    timerDisplay.textContent = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  if (totalMilliseconds >= 1000) {
+    second = (totalMilliseconds - minute * 60000) / 1000
   }
 
-  function buttonDisplay() {
-    if (isPaused && countdown === 0) {
-      startBtn.textContent = "START";
-    } else if (isPaused && countdown !== 0) {
-      startBtn.textContent = "Continue"; 
-    } else {
-      startBtn.textContent = "Pause";
-    }
-  }
+  populateFormatPomodoro(minute, second)
 
+  if (totalMilliseconds == 0) pomodoroComplete()
+ };
+
+ function pomodoroComplete() {
+  pausePomodoro()
+  startOrPauseAPomodoroFromListButton.disabled = true
+  setTimeout(`alert("Time is up")`, 100)
+ };
+
+ function populateFormatPomodoro(minute, second) {
+   let formatMinute = minute
+   let formatSecond = second
+
+   if (minute == 0) {
+     formatMinute = "00"
+   }
+
+   if (minute < 10 && minute > 0) {
+     formatMinute = "0" + minute
+   }
+
+   if (second < 10) {
+     formatSecond = "0" + second
+   }
+
+   pomodoroCounterDisplayElement.innerText = `${formatMinute}:${formatSecond}`
+   return [formatMinute, formatSecond]
+};
   
